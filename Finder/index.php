59be659adf6347dadf93bd0b4920aab8ed40d35b
nst27 <nishu314@gmail.com>
2025-02-02 19:45:06 +0530

    Added new title

--- Finder/index.php
+++ Finder/index.php
@@ -15,7 +15,7 @@ if(isset($_POST['submit'])){
 <html>
 
 <head>
-    <title>NISHANT</title>
+    <title>NST</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSS only -->
